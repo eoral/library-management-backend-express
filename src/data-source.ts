806633018e92ca12849a5110af0b0217bc7fd075
Eray Oral <erayoral@gmail.com>
2025-04-10 14:18:51 +0300

    Put DB params into dotenv file

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,3 +1,4 @@
+import dotenv from 'dotenv';
 import "reflect-metadata";
 import {DataSource} from "typeorm";
 import {User} from "./entity/user";
@@ -5,13 +6,17 @@ import {Book} from "./entity/book";
 import {BookBorrowHistory} from "./entity/book-borrow-history";
 import {BookScore} from "./entity/book-score";
 
+dotenv.config();
+
+const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
+
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "pass123",
-    database: "postgres",
+    host: DB_HOST,
+    port: parseInt(DB_PORT || "5432"),
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
     synchronize: false,
     logging: true,
     entities: [User, Book, BookBorrowHistory, BookScore],
